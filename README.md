8a5082ef13fc21105b41ca11e8a72839e4507b45
Blaine Motsinger <blaine@renderorange.com>
2026-04-01 00:18:04 -0500

    GH-1: add initial extension scaffolding

--- README.md
+++ README.md
@@ -1,1 +1,58 @@
-# rt-extension-lumentheme
+# NAME
+
+RT-Extension-Lumen
+
+# DESCRIPTION
+
+Lumen is a minimal, modern, and lightweight theme for RT.
+
+# RT VERSION
+
+Works with RT 6.0.0 and newer.
+
+# INSTALLATION
+
+- `perl Makefile.PL`
+- `make`
+- `make install`
+
+- Edit your `/opt/rt6/etc/RT_SiteConfig.pm`
+
+    Add this line:
+
+        Plugin( 'RT::Extension::Lumen' );
+
+- Clear your mason cache
+
+        rm -rf /opt/rt6/var/mason_data/obj
+
+- Restart your webserver
+
+# FEATURES
+
+Lumen rewrites RT's frontend from the ground up to:
+
+- remove dependencies on the RT elevator themes
+- replace the top and sub navigation menus with a single sidebar navigation menu
+- better support dynamic layout changes over a wide range of devices
+- simplify the user interface with minimal, unobtrusive, and uniform UI elements
+- update light and dark modes with softer colors
+
+Lumen still supports modern RT features like inline edit and htmx, but strips away complexity for a simplified and accessible user experience.
+
+# BUGS AND FEATURE REQUESTS
+
+Please report bugs and feature requests at [GitHub](https://github.com/renderorange/rt-extension-lumen/issues).
+
+# AUTHOR
+
+Blaine Motsinger <blaine@renderorange.com>
+
+# LICENSE AND COPYRIGHT
+
+Copyright (c) 2026 Blaine Motsinger
+
+This is free software, licensed under:
+
+    The GNU General Public License, Version 2, June 1991
+
---
+++ lib/RT/Extension/Lumen.pm
@@ -0,0 +1,83 @@
+use strict;
+use warnings;
+package RT::Extension::Lumen;
+
+our $VERSION = '0.001';
+
+=pod
+
+=head1 NAME
+
+RT-Extension-Lumen
+
+=head1 DESCRIPTION
+
+Lumen is a minimal, modern, and lightweight theme for RT.
+
+=head1 RT VERSION
+
+Works with RT 6.0.0 and newer.
+
+=head1 INSTALLATION
+
+=over
+
+=item C<perl Makefile.PL>
+
+=item C<make>
+
+=item C<make install>
+
+=item Edit your F</opt/rt5/etc/RT_SiteConfig.pm>
+
+Add this line:
+
+    Plugin( 'RT::Extension::Lumen' );
+
+=item Clear your mason cache
+
+    rm -rf /opt/rt6/var/mason_data/obj
+
+=item Restart your webserver
+
+=back
+
+=head1 FEATURES
+
+Lumen rewrites RT's frontend from the ground up to:
+
+=over
+
+=item remove dependencies on Bootstrap, jQuery, and the RT elevator themes
+
+=item replace the top and sub navigation menus with a single sidebar navigation menu
+
+=item better support dynamic layout changes over a wide range of devices
+
+=item simplify the user interface with minimal, unobtrusive, and uniform UI elements
+
+=item update light and dark modes with softer colors
+
+=back
+
+Lumen still supports modern RT features like inline edit and htmx, but strips away complexity for a simplified and accessible user experience.
+
+=head1 BUGS AND FEATURE REQUESTS
+
+Please report bugs and feature requests at L<GitHub|https://github.com/renderorange/rt-extension-lumen/issues>.
+
+=head1 AUTHOR
+
+Blaine Motsinger E<lt>blaine@renderorange.comE<gt>
+
+=head1 LICENSE AND COPYRIGHT
+
+Copyright (c) 2026 Blaine Motsinger.
+
+This is free software, licensed under:
+
+    The GNU General Public License, Version 2, June 1991
+
+=cut
+
+1;
